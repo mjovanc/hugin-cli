74a22c11d012c3f9713f7d18f73a39e5deb0a68b
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-21 23:18:10 +0200

    Restructuring packages

--- config/cli_header.h
+++ config/cli_header.h
@@ -31,21 +31,21 @@
 #include <config/ascii.h>
 #include <version.h>
 
-namespace kryptokrona
+namespace hugin
 {
   inline std::string getProjectCLIHeader()
   {
     std::stringstream programHeader;
     programHeader << std::endl
       << asciiArt << std::endl
-      << " " << hugin::parameters::HUGIN_NAME << " v" << PROJECT_VERSION_LONG << std::endl
+      << " " << hugin::HUGIN_NAME << " v" << PROJECT_VERSION_LONG << std::endl
       << " This software is distributed under the General Public License v3.0"
       << std::endl << std::endl
       << " " << PROJECT_COPYRIGHT
       << std::endl << std::endl
       << " Additional Copyright(s) may apply, please see the included LICENSE file for more information." << std::endl
       << " If you did not receive a copy of the LICENSE, please visit:" << std::endl
-      << " " << hugin::parameters::LICENSE_URL
+      << " " << hugin::LICENSE_URL
       << std::endl << std::endl;
 
     return programHeader.str();
