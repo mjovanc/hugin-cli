0658acc91b34555e8bdbb825ce9f87bd04859c79
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-10-12 09:49:09 +0200

    Fixing imports

--- db/database.cpp
+++ db/database.cpp
@@ -26,9 +26,7 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <stdlib.h>
 #include <iostream> 
-#include <sstream>
 
 #include "db/sqlite3.h"
 #include "database.h"
