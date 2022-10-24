7216e41b9274deaa7e22b21838355f94eee2c515
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-10-24 17:02:17 +0200

    Changing C standard

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 // Copyright (c) 2022-2022, The Kryptokrona Project
-// 
+//
+// Created by Marcus Cvjeticanin
+//
 // All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without modification, are
@@ -28,5 +30,6 @@
 
 int main(int argc, char *argv[])
 {
+    printf("argc = %d\n", argc);
     return 0;
 }
