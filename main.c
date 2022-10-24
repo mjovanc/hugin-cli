0246e27571242e06f8173f84ded9f090e0e0fcd5
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-10-24 17:42:39 +0200

    Adding missing include

--- main.c
+++ main.c
@@ -28,6 +28,8 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#inlcude <stdio.h>
+
 int main(int argc, char *argv[])
 {
     printf("argc = %d\n", argc);
