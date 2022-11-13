a9d222df8375203189e825d0b9c2752fdf6afa13
mjovanc <mjovanc@icloud.com>
2022-11-13 02:07:46 +0100

    Modifying wallet_create function

--- wallet/wallet.c
+++ wallet/wallet.c
@@ -46,7 +46,7 @@
 int wallet_create(char *wallet_name, char *wallet_password)
 {
     char *extension = ".db";
-    size_t tmp_wallet_size = sizeof(wallet_name) + strlen(extension);
+    const unsigned long tmp_wallet_size = strlen(wallet_name) + strlen(extension);
     char tmp_wallet_name[tmp_wallet_size];
 
     strncpy(tmp_wallet_name, wallet_name, tmp_wallet_size);
